f1e72de703951675ec27e59108a71fcbb1e66573
Druzhkov Nikolay <diyavolik007@yandex.ru>
2024-10-21 21:26:11 +0300

    задание 11 спринта реализовано в полном объёме

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,39 +6,124 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
+    enum class ErrorSearch {
+        NOT_FOUND,
+        CONNECTION_PROBLEMS,
+        INVISIBLE
+    }
 
     companion object {
         const val SAVE_VALUE = "SAVE_VALUE"
         const val DEFAULT_VALUE = ""
+        const val BASEURL = "https://itunes.apple.com"
     }
 
     private var inputValue: String = DEFAULT_VALUE
 
-    @SuppressLint("ServiceCast")
+    private val iTunesBaseUrl = BASEURL
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+
+    private lateinit var clearButton: ImageView
+    private lateinit var inputEditText: EditText
+    private lateinit var trackList: RecyclerView
+    private lateinit var toolbar: Toolbar
+    private lateinit var layoutSearchError: LinearLayout
+    private lateinit var errorImage: ImageView
+    private lateinit var errorMessage: TextView
+    private lateinit var buttonUpdateErrorSearch: Button
+
+    private val adapter = TrackAdapter(mutableListOf())
+
+    private fun requestTrack(searchInput: String) {
+        itunesService.search(searchInput).enqueue(object :
+            Callback<ItunesResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<ItunesResponse>,
+                response: Response<ItunesResponse>
+            ) {
+                if (response.code() == 200) {
+                    if ((response.body()?.resultCount ?: 0) > 0) {
+                        searchProblems(ErrorSearch.INVISIBLE)
+                        adapter.tracks.clear()
+                        adapter.tracks.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                    } else {
+                        searchProblems(ErrorSearch.NOT_FOUND)
+                    }
+                } else {
+                    searchProblems(ErrorSearch.NOT_FOUND)
+                }
+            }
+
+            override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
+                searchProblems(ErrorSearch.CONNECTION_PROBLEMS)
+            }
+        })
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun searchProblems(errorSearch: ErrorSearch) {
+        if (adapter.tracks.isNotEmpty()) {
+            adapter.tracks.clear()
+            adapter.notifyDataSetChanged()
+        }
+        when (errorSearch) {
+            ErrorSearch.NOT_FOUND -> {
+                layoutSearchError.visibility = View.VISIBLE
+                errorImage.setImageResource(R.drawable.nothing_found)
+                errorMessage.text = getString(R.string.error_search_nothing_found)
+                buttonUpdateErrorSearch.visibility = View.GONE
+            }
+
+            ErrorSearch.CONNECTION_PROBLEMS -> {
+                layoutSearchError.visibility = View.VISIBLE
+                errorImage.setImageResource(R.drawable.connection_problems)
+                errorMessage.text = getString(R.string.error_search_connection_problems)
+                buttonUpdateErrorSearch.visibility = View.VISIBLE
+            }
+
+            else -> layoutSearchError.visibility = View.GONE
+        }
+    }
+
+
+    @SuppressLint("ServiceCast", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val trackList: List<Track> = listOf(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg "),
-        )
-
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerTrackView)
-        val toolbar: Toolbar = findViewById(R.id.toolbar_search)
+        clearButton = findViewById(R.id.clearIcon)
+        inputEditText = findViewById(R.id.inputEditText)
+        trackList = findViewById(R.id.recyclerTrackView)
+        toolbar = findViewById(R.id.toolbar_search)
+        layoutSearchError = findViewById(R.id.layoutSearchError)
+        errorImage = findViewById(R.id.errorImage)
+        errorMessage = findViewById(R.id.errorMessage)
+        buttonUpdateErrorSearch = findViewById(R.id.buttonUpdateErrorSearch)
         setSupportActionBar(toolbar)
 
         toolbar.setNavigationIcon(R.drawable.vector_arrow_back)
@@ -55,6 +140,12 @@ class SearchActivity : AppCompatActivity() {
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+            adapter.tracks.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        buttonUpdateErrorSearch.setOnClickListener {
+            requestTrack(inputEditText.text.toString())
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -77,7 +168,17 @@ class SearchActivity : AppCompatActivity() {
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        recyclerView.adapter = TrackAdapter(trackList)
+        trackList.adapter = adapter
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (inputEditText.text.isNotEmpty()) {
+                    requestTrack(inputEditText.text.toString())
+                }
+                true
+            }
+            false
+        }
 
     }
 
--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -2,16 +2,23 @@ package com.example.playlistmaker
 
 import android.content.Context
 import android.util.TypedValue
-import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 
 class TrackViewHolder (
-    itemView: View
-) : RecyclerView.ViewHolder(itemView){
+    parent: ViewGroup
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.track_view, parent, false))
+{
     private val sourceArtwork: ImageView = itemView.findViewById(R.id.artwork)
     private val sourceTrackName: TextView = itemView.findViewById(R.id.trackName)
     private val sourceArtistName: TextView = itemView.findViewById(R.id.artistName)
@@ -19,14 +26,15 @@ class TrackViewHolder (
 
     fun bind(model: Track){
         val imageUrl = model.artworkUrl100
-        Glide.with(itemView)
+        Glide.with(itemView.context)
             .load(imageUrl)
             .placeholder(R.drawable.ic_placeholder)
             .transform(RoundedCorners(dpToPx(RADIUS_IMAGE, itemView.context)))
             .into(sourceArtwork)
         sourceTrackName.text= model.trackName
         sourceArtistName.text= model.artistName
-        sourceTrackTime.text= model.trackTime
+        sourceTrackTime.text= SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis)
+        sourceArtistName.requestLayout()
     }
     private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
