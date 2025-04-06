3909b38c03f91621617301bbf70df4f4b467267f
Druzhkov Nikolay <diyavolik007@yandex.ru>
2025-04-06 20:57:45 +0300

    медиатека переведена на Fragment

--- app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
-import com.example.playlistmaker.medialibrary.ui.activity.MediaLibraryActivity
+import com.example.playlistmaker.medialibrary.ui.fragment.MediaLibraryFragment
 import com.example.playlistmaker.search.ui.activity.SearchActivity
 import com.example.playlistmaker.settings.ui.fragment.SettingsFragment
 
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         val buttonMediaLibrary = findViewById<Button>(R.id.button_media_library)
         buttonMediaLibrary.setOnClickListener {
-            val displayIntent = Intent(this, MediaLibraryActivity::class.java)
+            val displayIntent = Intent(this, MediaLibraryFragment::class.java)
             startActivity(displayIntent)
         }
 
---
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/MediaLibraryFragment.kt
@@ -0,0 +1,64 @@
+package com.example.playlistmaker.medialibrary.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentMediaLibraryBinding
+import com.example.playlistmaker.medialibrary.ui.viewmodel.MediaLibraryViewModel
+import com.google.android.material.tabs.TabLayoutMediator
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MediaLibraryFragment : Fragment() {
+
+    private val viewModel: MediaLibraryViewModel by viewModel<MediaLibraryViewModel>()
+
+    private lateinit var binding: FragmentMediaLibraryBinding
+    private lateinit var tabMediator: TabLayoutMediator
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMediaLibraryBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMediaLibraryBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        setSupportActionBar(binding.toolbarMediaLibrary)
+//        binding.toolbarMediaLibrary.setNavigationIcon(R.drawable.vector_arrow_back)
+//        binding.toolbarMediaLibrary.setNavigationOnClickListener {
+//            finish()
+//        }
+
+        binding.viewPager.adapter = MediaLibraryPagerAdapter(
+            fragmentManager = childFragmentManager,
+            lifecycle = lifecycle)
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) {tab, position ->
+            when(position){
+                0 -> tab.text = getString(R.string.favourite_track)
+                1 -> tab.text = getString(R.string.playlist)
+            }
+        }
+        tabMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
+}
