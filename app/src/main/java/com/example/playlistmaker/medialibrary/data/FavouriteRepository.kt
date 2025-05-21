811fc50c117ed6da011623b92b78b59ce6ff5b62
Druzhkov Nikolay <diyavolik007@yandex.ru>
2025-05-21 20:03:57 +0300

    реализован слой UI

--- app/src/main/java/com/example/playlistmaker/medialibrary/data/FavouriteRepository.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/data/FavouriteRepository.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker.medialibrary.data
 
-import com.example.playlistmaker.player.domain.model.TrackFavourite
+
+import com.example.playlistmaker.medialibrary.domain.model.TrackFavourite
 import kotlinx.coroutines.flow.Flow
 
 interface FavouriteRepository {
--- app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/FavouriteTrackFragment.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/FavouriteTrackFragment.kt
@@ -7,6 +7,11 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
+import com.example.playlistmaker.application.TrackAdapter
+import com.example.playlistmaker.application.debounce
 import com.example.playlistmaker.databinding.FragmentFavouriteTrackBinding
 import com.example.playlistmaker.medialibrary.domain.model.TrackFavourite
 import com.example.playlistmaker.medialibrary.ui.state.FavouriteData
@@ -16,12 +21,16 @@ class FavouriteTrackFragment : Fragment() {
 
     companion object {
         fun newInstance() = FavouriteTrackFragment()
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
     }
 
     val viewModel by activityViewModels<FavouriteTrackViewModel>()
 
     private var _binding: FragmentFavouriteTrackBinding? = null
     private val binding get() = _binding!!
+
+    private var adapter: TrackAdapter<TrackFavourite>? = null
+    private lateinit var onTrackClickDebounce: (TrackFavourite) -> Unit
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,12 +44,28 @@ class FavouriteTrackFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        adapter = TrackAdapter {onTrackClickDebounce}
+
+        onTrackClickDebounce = debounce<TrackFavourite>(
+            delayMillis = CLICK_DEBOUNCE_DELAY,
+            coroutineScope = viewLifecycleOwner.lifecycleScope,
+            useLastParam = false,
+        ) {findNavController().navigate(
+                R.id.action_mediaLibraryFragment_to_audioPlayerFragment2
+        )
+        }
+
         viewModel.getFavouriteLiveData().observe(viewLifecycleOwner) { favouriteList ->
             when (favouriteList) {
-                is FavouriteData.Content -> showFavouriteList(favouriteList)
+                is FavouriteData.Content -> {
+                    val trackListFavourite = favouriteList.favouriteTrackList
+                    showFavouriteList(trackListFavourite)
+                }
                 is FavouriteData.Empty -> showPlaceHolder()
             }
         }
+
+        binding.recyclerTrackView.adapter = adapter
 
     }
 
@@ -50,10 +75,12 @@ class FavouriteTrackFragment : Fragment() {
         binding.recyclerTrackView.isVisible = false
     }
 
-    private fun showFavouriteList(favouriteList: FavouriteData) {
+    private fun showFavouriteList(trackListFavourite: List<TrackFavourite>) {
+        adapter?.clearOrUpdateTracks()
         binding.errorImage.isVisible = false
         binding.errorMessage.isVisible = false
         binding.recyclerTrackView.isVisible = true
+        adapter?.allUpdateTracks(trackListFavourite)
     }
 
     override fun onDestroyView() {
