a0fbaa28de67f352988b45c98d0b630cfa579486
Druzhkov Nikolay <diyavolik007@yandex.ru>
2025-06-03 20:42:56 +0300

    реализован переход "Медиатека"-"Плейлисты"

--- app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/NewPlayListFragment.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/NewPlayListFragment.kt
@@ -1,5 +1,52 @@
 package com.example.playlistmaker.medialibrary.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentNewPlaylistBinding
 
-class NewPlayListFragment: Fragment()
+class NewPlayListFragment: Fragment() {
+
+    private var _binding: FragmentNewPlaylistBinding? = null
+    private val binding get() = _binding!!
+
+    private fun toolBar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarNewPlaylist)
+
+        binding.toolbarNewPlaylist.setNavigationIcon(R.drawable.vector_arrow_back)
+        binding.toolbarNewPlaylist.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(true)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNewPlaylistBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolBar()
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/PlayListFragment.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragment/PlayListFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistBinding
 import com.example.playlistmaker.medialibrary.ui.viewmodel.PlayListViewModel
 
@@ -29,6 +31,16 @@ class PlayListFragment: Fragment(){
         _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonNewPlayList.setOnClickListener{
+            findNavController().navigate(
+                R.id.action_mediaLibraryFragment_to_newPlayListFragment
+            )
+        }
     }
 
     override fun onDestroyView() {
